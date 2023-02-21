ec044953f65de5f58464421b82c1678975403d8d
Juan Versolato Lopes <30662756+Truta446@users.noreply.github.com>
2023-02-21 20:36:47 -0300

    Create hello.asm

---
+++ hello.asm
@@ -0,0 +1,18 @@
+global _start
+
+section .text
+
+_start:
+    MOV rax, 0x1
+    MOV rdi, 0x1
+    MOV rsi, msg
+    MOV rdx, msglength
+    SYSCALL
+
+    MOV rax, 0x3C
+    MOV rdi, 0x0
+    SYSCALL
+
+section .data
+    msg: DB "hello world", 0xA
+    msglength: EQU $ - msg
